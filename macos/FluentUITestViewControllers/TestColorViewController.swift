1d1bf4ebf5a3ba25c2d41dbff41c8732344ab321
Harrie Shin <hyshin@microsoft.com>
2022-08-09 20:57:19 -0700

    update and create classes

--- macos/FluentUITestViewControllers/TestColorViewController.swift
+++ macos/FluentUITestViewControllers/TestColorViewController.swift
@@ -28,7 +28,7 @@ class TestColorViewController: NSViewController {
 		primaryColorsStackView.alignment = .leading
 
 		for color in Colors.Palette.allCases {
-			colorsStackView.addArrangedSubview(createColorRowStackView(name: color.name, color: color.color))
+			colorsStackView.addArrangedSubview(ColorInfoView(name: color.name, color: color.color))
 		}
 
 		loadPrimaryColors(state: NSControl.StateValue.off)
@@ -78,18 +78,6 @@ class TestColorViewController: NSViewController {
 		view = containerView
 	}
 
-	private func createColorRowStackView(name: String?, color: NSColor?) -> NSStackView {
-		let rowStackView = NSStackView()
-		let textView = NSTextField(labelWithString: name!)
-		let primaryColorView = ColorRectView(color: color!)
-		textView.font = .systemFont(ofSize: 18)
-		rowStackView.orientation = .horizontal
-		rowStackView.spacing = 20.0
-		rowStackView.addArrangedSubview(primaryColorView)
-		rowStackView.addArrangedSubview(textView)
-		return rowStackView
-	}
-
 	@objc private func toggleClicked(button: NSSwitch?) {
 		primaryColorsStackView.subviews.removeAll()
 		loadPrimaryColors(state: button?.state ?? NSControl.StateValue.off)
@@ -118,14 +106,14 @@ class TestColorViewController: NSViewController {
 			toggleTextView.string = "Default"
 		}
 
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primary", color: Colors.primary))
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primaryShade10", color: Colors.primaryShade10))
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primaryShade20", color: Colors.primaryShade20))
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primaryShade30", color: Colors.primaryShade30))
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primaryTint10", color: Colors.primaryTint10))
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primaryTint20", color: Colors.primaryTint20))
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primaryTint30", color: Colors.primaryTint30))
-		primaryColorsStackView.addArrangedSubview(createColorRowStackView(name: "primaryTint40", color: Colors.primaryTint40))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primary", color: Colors.primary))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primaryShade10", color: Colors.primaryShade10))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primaryShade20", color: Colors.primaryShade20))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primaryShade30", color: Colors.primaryShade30))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primaryTint10", color: Colors.primaryTint10))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primaryTint20", color: Colors.primaryTint20))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primaryTint30", color: Colors.primaryTint30))
+		primaryColorsStackView.addArrangedSubview(ColorInfoView(name: "primaryTint40", color: Colors.primaryTint40))
 		NSLayoutConstraint.activate(subviewConstraints)
 	}
 }
@@ -154,3 +142,37 @@ class ColorRectView: NSView {
 }
 
 private let colorRowSpacing: CGFloat = 10.0
+
+class ColorInfoView: NSStackView, NSAccessibilityRow {
+	private let textView: NSTextField
+	private let primaryColorView: ColorRectView
+
+	init(name: String?, color: NSColor?) {
+		super.init(frame: .zero)
+		textView = NSTextField(labelWithString: name!)
+		primaryColorView = ColorRectView(color: color!)
+		textView.font = .systemFont(ofSize: 18)
+		orientation = .horizontal
+		spacing = 20.0
+		addArrangedSubview(primaryColorView)
+		addArrangedSubview(textView)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+class ColorScrollView : NSScrollView, NSAccessibilityTable {
+	lazy var primaryColorsStackView: NSStackView = {
+		var primaryColorsStackView = NSStackView()
+		primaryColorsStackView.translatesAutoresizingMaskIntoConstraints = false
+		primaryColorsStackView.orientation = .vertical
+		primaryColorsStackView.alignment = .leading
+		return primaryColorsStackView
+	}()
+
+	func accessibilityRows() -> [NSAccessibilityRow]? {
+		return primaryColorsStackView.arrangedSubviews as [ColorInfoView]
+	}
+}
